02fc2a1cffa5ef7123af415855a3f44248ff0e7d
Jesse Cooper <jesse.cooper@joinroot.com>
2019-11-08 11:15:22 -0600

    Add JS <-> Native Bridge to make SDK methods accessible

--- react-native-drive-science-demo-library/index.js
+++ react-native-drive-science-demo-library/index.js
@@ -1,12 +1,8 @@
 import { NativeModules } from "react-native";
-
 const { DriveScienceDemoLibrary } = NativeModules;
-
-export default DriveScienceDemoLibrary;
 
 export const Environment = { STAGING: "staging", PRODUCTION: "production" };
 
 export function initRootSDK(clientId, rootDriverToken, environment, callback) {
-  const { RootSDK } = NativeModules;
-  RootSDK.initRootSDK(clientId, rootDriverToken, environment, callback);
+  DriveScienceDemoLibrary.initRootSDK(clientId, rootDriverToken, environment, callback);
 }
